2c8ec3df3cad23fd666b1578698dfef3aeeb0567
Asma Ben Ahmed <asma.ahmed@driveterra.com>
2025-06-06 00:41:43 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-gray-800">
+        Rem Waste Company
+      </h1>
+    </div>
+  )
+}
+
+export default App
